d8d178b76f1cc85f42c81774ca919e0346d623d7
Ujjwal Kumar <ujjwalk863@gmail.com>
2026-02-10 18:05:54 +0530

    second real commit

--- 02_conversion_operations/01_conversion_operations.js
+++ 02_conversion_operations/01_conversion_operations.js
@@ -1,1 +1,10 @@
-console.log("hello");
+//  here we are going to learn about explicit type conversion
+let score  = "33"
+
+console.log(typeof score);
+console.log(typeof(score));
+
+// now we want ki hamara jo score hai woh number me badal jaye
+
+let score_num = Number(score) // hmare hamesha capital me Number ka first letter likha hai kyuki woh ek class hai 
+console.log(typeof(score_num));
