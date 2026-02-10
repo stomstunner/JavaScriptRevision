78da4f706d09ab8972d6c9ad7d9c203cd31b7b7a
Ujjwal Kumar <ujjwalk863@gmail.com>
2026-02-10 19:00:31 +0530

    first commit

---
+++ 02_conversion_operations/04_conversion_string.js
@@ -0,0 +1,6 @@
+//  here we see how we can convert the number into string 
+let some_num = 978679
+let num_to_str = String(some_num)
+console.log(typeof num_to_str); // string
+console.log(num_to_str); // value is in string but printed as a number 
+
