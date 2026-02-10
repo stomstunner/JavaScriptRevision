b0fd5b36c8c10152a884d431ba57cd6c0df6ddb1
Ujjwal Kumar <ujjwalk863@gmail.com>
2026-02-10 18:21:06 +0530

    last commit

--- 02_conversion_operations/01_conversion_operations.js
+++ 02_conversion_operations/01_conversion_operations.js
@@ -8,3 +8,16 @@ console.log(typeof(score));
 
 let score_num = Number(score) // hmare hamesha capital me Number ka first letter likha hai kyuki woh ek class hai 
 console.log(typeof(score_num));
+
+
+// in javascript aer koi value hai aur woh number me convert nahi paa rarhi hai toh type nikalne pe uska answer NaN aata hai means not a number 
+let marks ="234ac"
+console.log(typeof(marks));
+// now we convert this value to a number
+let marks_num = Number(marks)
+console.log(typeof(marks_num)); // ab yaha pe number aa rha hai answer
+console.log(marks_num); // but jab ham print karwane ka try karte hai toh woh NaN aa jata hai
+
+
+
+
