4d04759af30392ed81f2202e63154054f9b0e25d
Ujjwal Kumar <ujjwalk863@gmail.com>
2026-02-10 19:11:12 +0530

    first commit basic arthemtic operation

---
+++ 03_operators/01_arthmetic_operations.js
@@ -0,0 +1,8 @@
+// lets just finish the simple and besic arthemtic opertions
+
+console.log("add",2+2);
+console.log("sub",2-2);
+console.log("multi",2*2);
+console.log("dev",2/2);
+console.log("modulo",2%2);
+console.log("power",2**3);
