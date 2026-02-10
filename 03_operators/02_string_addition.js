caadb9f14812a9540ad648289a5a49543ec77d1f
Ujjwal Kumar <ujjwalk863@gmail.com>
2026-02-10 19:14:59 +0530

    first commit

---
+++ 03_operators/02_string_addition.js
@@ -0,0 +1,7 @@
+// lets see the basic string addition
+let str1 = "Hello"
+let str2 = " Ujjwal"
+let str3 = str1 + str2
+// now we can print the Hello World by just adding the both str1 and str2 
+
+console.log(str3) // Hello Ujjwal 
