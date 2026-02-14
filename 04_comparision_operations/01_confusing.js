da59cfcd9d9a5eb43a36bd58b2044fd1795b2028
Ujjwal Kumar <ujjwalk863@gmail.com>
2026-02-14 20:38:11 +0530

    first commit

---
+++ 04_comparision_operations/01_confusing.js
@@ -0,0 +1,8 @@
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
+
+// The reason is that an equality check == and comparisons > <>=
+// <= work differently.
+// Comparisons convert null to a number, treating it as 0.
+// That's why (3) null >= 0 is true and (1) null > 0 is false.
