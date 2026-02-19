0e0812764abae52b02b90e5863863aaa9b4bc28e
Ujjwal Kumar <ujjwalk863@gmail.com>
2026-02-19 18:52:01 +0530

    2nd commit

--- 10_array/03_conversion_array.js
+++ 10_array/03_conversion_array.js
@@ -1,1 +1,27 @@
 // we can convert and ask ki koi bhi data array hai ya nahi hai 
+// console.log(Array.isArray("Ujjwal")); // false
+
+// console.log(Array.isArray([61253671253,172721654,7625467235])); // true
+
+// we can convert objects,int,string and can convert it to an array 
+// console.log(Array.from("Ujjwal is a good boy"));
+
+
+let name = "ujjwal"
+let name2 = 24
+
+console.log(Array.from(name2));
+console.log(Array.from(name2));// when it can convert to array then it retun empty array 
+
+
+// we ahve another method for multiple varible ko array me badalne ke liye 
+
+let a = 12
+let b = 13
+let c = 15
+console.log(Array.of(a, b, c)); //[ 12, 13, 15 ] 
+
+
+
+
+
