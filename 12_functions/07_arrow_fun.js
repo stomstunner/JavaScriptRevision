3e5927b62c1ccea6083de97fbffc44d0976c2fab
Ujjwal Kumar <ujjwalk863@gmail.com>
2026-02-24 17:28:46 +0530

    first commit

---
+++ 12_functions/07_arrow_fun.js
@@ -0,0 +1,26 @@
+// lets discuss about the arrow fucntion  
+//  () => {}  yes this is the arrow function  
+//  we can hold the arrow function in any other variable also 
+
+// function user(){
+//     let username = "ujjwal;
+//     console.log(`Hello ${username}`);
+    
+// }
+
+// so that is just a arrow fucntion but we do not write the fucntion name and and keyword and store it in a varibale  
+
+
+let user = () => {
+    let username = "Ujjwal";
+    console.log(`Hello ${username}`);
+    
+}
+// user()
+
+// lets make a simple fucntion and then we convert it into arrow fucntion 
+function addtwo(num1, num2){
+    return num1 + num2
+}
+
+
