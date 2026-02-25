3a1bda5ebfa2c9bdef4920f3a3429d5a95cd704a
Ujjwal Kumar <ujjwalk863@gmail.com>
2026-02-25 20:41:40 +0530

    first commit

---
+++ 14_ittrations/02_array_loop.js
@@ -0,0 +1,7 @@
+// loop on array 
+let arr = ["Superman", "Batman", "Ironman", "Spiderman", "Antman"]
+for(let i = 0;i<arr.length;i++){
+    console.log(arr[i]);
+    // we go out of bound of  the array so at last we get a undefined value
+    // so we run loop from 0 to n-1
+}
