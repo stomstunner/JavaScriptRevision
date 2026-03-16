2dc070b08a2299fd2e63a20f627f6994140032d9
Ujjwal Kumar <ujjwalk863@gmail.com>
2026-03-16 20:26:02 +0530

    first commit

---
+++ 14_ittrations/07_loops_on_strings.js
@@ -0,0 +1,6 @@
+let gretting = "Hello World"
+for ( const greet of gretting){
+    console.log(`${greet} is a single char`);
+    
+}
+// here greet is a itrrator variable jo ki ittrate hote rehta hai greeting ke har ek single character ke uper 
