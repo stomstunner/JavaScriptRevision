0d209f68f98ea55b8ce72b110ca81f91c85c0234
Ujjwal Kumar <ujjwalk863@gmail.com>
2026-03-16 20:26:22 +0530

    first commit

---
+++ 14_ittrations/06_loops_on_obj.js
@@ -0,0 +1,12 @@
+// loops on objects
+let obj = {
+    name : "Ujjwal",
+    roll : 234,
+    age : 21,
+    class : "BCA"
+}
+// we have to use for in loop for objects
+for( let key in obj){
+    console.log(key);
+    // isse ham keys ko print kar sakte hai using the for in loop 
+}
