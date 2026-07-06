1eda4da0223853594aeeddf6957174b8687cb2d1
Ujjwal Kumar <ujjwalk863@gmail.com>
2026-07-06 14:50:25 +0530

    first commit

---
+++ 14_ittrations/10_array_access.js
@@ -0,0 +1,8 @@
+// in the javascript when we use the for in loop we have access to all the things in the array like item , index and the whole array for that perticular item
+
+let arr = ['ujjwal1','ujjwal2','ujjwal3','ujjwal4','ujjwal5']
+// here we use the for each loop in the arr array 
+arr.forEach(( index, arr)=>{
+    console.log( index, arr);
+    // ok it take the 3 argument 1st one if or the item or element and the 2nd one if for the index and the 3red on is for the whole array 
+})
